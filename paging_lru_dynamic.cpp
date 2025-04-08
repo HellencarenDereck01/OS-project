620703decfb00888747c7fb6b1ec32a670f7811f
HellencarenDereck01 <152360619+HellencarenDereck01@users.noreply.github.com>
2025-04-08 20:36:04 +0530

    dynamic LRU paging
    Implemented a dynamic paging system using the Least Recently Used (LRU) page replacement algorithm. This system allocates memory frames dynamically and maintains a record of page access history to determine which pages to evict upon a page fault.

---
+++ paging_lru_dynamic.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+#include <list>
+using namespace std;
+
+int main() {
+    int capacity, n;
+
+    cout << "Enter number of memory frames: ";
+    cin >> capacity;
+
+    cout << "Enter number of page references: ";
+    cin >> n;
+
+    int pages[n];
+    cout << "Enter the page reference string:\n";
+    for (int i = 0; i < n; ++i) {
+        cin >> pages[i];
+    }
+
+    unordered_set<int> memory;
+    unordered_map<int, list<int>::iterator> positions;
+    list<int> lruList;
+
+    int pageFaults = 0;
+
+    for (int i = 0; i < n; ++i) {
+        int page = pages[i];
+        cout << "Accessed Page: " << page;
+
+        if (memory.find(page) == memory.end()) {
+            // Page fault
+            pageFaults++;
+            cout << " (Page Fault!)";
+
+            if (memory.size() == capacity) {
+                int lruPage = lruList.back();
+                lruList.pop_back();
+                memory.erase(lruPage);
+                positions.erase(lruPage);
+                cout << " - Removed LRU Page: " << lruPage;
+            }
+
+            memory.insert(page);
+        } else {
+            // Page hit, remove old usage
+            lruList.erase(positions[page]);
+        }
+
+        // Move to front (most recently used)
+        lruList.push_front(page);
+        positions[page] = lruList.begin();
+
+        // Show current memory
+        cout << "\nCurrent Memory: ";
+        for (int val : lruList)
+            cout << val << " ";
+        cout << "\n\n";
+    }
+
+    cout << "Total Page Faults (LRU): " << pageFaults << endl;
+
+    return 0;
+}
